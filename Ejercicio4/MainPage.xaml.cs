a6ae79b98f1ea1526fc310bcec4afe4c680bbab9
almarpa <alex_marco7@outlook.es>
2020-01-12 17:51:20 +0100

    Ejercicio 4 corregido

--- Ejercicio4/MainPage.xaml.cs
+++ Ejercicio4/MainPage.xaml.cs
@@ -26,5 +26,56 @@ namespace Ejercicio4
         {
             this.InitializeComponent();
         }
+
+        /** FIRST IMAGE **/
+        private void image_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            // Fijar la posición desde donde realizar las transformaciones
+            Image image = (Image)sender;
+            ct1.CenterX = image.ActualWidth / 2;
+            ct1.CenterY = image.ActualHeight / 2;
+        }
+
+        private void image_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+
+            FrameworkElement source = (FrameworkElement)e.OriginalSource;
+            
+            // Rotation
+            ct1.Rotation += e.Delta.Rotation;
+            // Scale
+            ct1.ScaleX /= e.Delta.Scale;
+            ct1.ScaleY /= e.Delta.Scale;
+            // Translation
+            ct1.TranslateX += e.Delta.Translation.X;
+            ct1.TranslateY += e.Delta.Translation.Y;
+
+            source.RenderTransform = ct1;
+        }
+
+        /** SECOND IMAGE **/
+        private void image_ImageOpened2(object sender, RoutedEventArgs e)
+        {
+            // Fijar la posición desde donde realizar las transformaciones
+            Image image = (Image)sender;
+            ct2.CenterX = image.ActualWidth / 2;
+            ct2.CenterY = image.ActualHeight / 2;
+        }
+
+        private void image_ManipulationDelta2(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            FrameworkElement source = (FrameworkElement)e.OriginalSource;
+            
+            // Rotation
+            ct2.Rotation += e.Delta.Rotation;
+            // Scale
+            ct2.ScaleX /= e.Delta.Scale;
+            ct2.ScaleY /= e.Delta.Scale;
+            // Translation
+            ct2.TranslateX += e.Delta.Translation.X;
+            ct2.TranslateY += e.Delta.Translation.Y;
+
+            source.RenderTransform = ct2;
+        }
     }
 }
