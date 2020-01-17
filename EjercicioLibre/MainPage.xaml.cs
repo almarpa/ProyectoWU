540c6b258b9ec48e3cf5db2448e3680fb793321c
almarpa <alex_marco7@outlook.es>
2020-01-17 17:41:35 +0100

    Ejercicio libre implementado

--- EjercicioLibre/MainPage.xaml.cs
+++ EjercicioLibre/MainPage.xaml.cs
@@ -17,19 +17,16 @@ using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
-
 namespace EjercicioLibre
 {
-    /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         int local = 0;
         int visitante = 0;
         List<CompositeTransform> listCTransform = new List<CompositeTransform>();
         List<Image> listImages = new List<Image>();
+        List<Boolean> isBall = new List<Boolean>();
 
         public MainPage()
         {
@@ -51,6 +48,7 @@ namespace EjercicioLibre
         private void image_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
             Image image = sender as Image;
+            // Buscamos la posición de la imagen en la lista de imágenes, para poder saber cuál es su CompositeTransform
             int pos = listImages.IndexOf(listImages.Find(x => x == image));
             CompositeTransform ct = listCTransform[pos];
 
@@ -63,51 +61,93 @@ namespace EjercicioLibre
             ct.TranslateX += e.Delta.Translation.X;
             ct.TranslateY += e.Delta.Translation.Y;
 
-            image.RenderTransform = ct;
-
-            if (e.IsInertial)
-            {
-                double x = e.Delta.Translation.X;
-                double y = e.Delta.Translation.Y;
-                /*
-                if(e.Delta.Translation.Y <= 703-246.1 && e.Delta.Translation.Y >  && e.Delta.Translation.X > grid.ActualWidth)
+            // Comprobamos que sea una pelota antes de contabilizar los goles
+            if (isBall[pos] == true) {
+                if (e.IsInertial)
                 {
-                    local = local + 1;
-                    marcador1.Text = local.ToString();
+                    // Para detectar el gol encajado en la portería del equipo visitante
+                    if (ct.TranslateY >= -70 && ct.TranslateY < 50 && ct.TranslateX > 770 && ct.TranslateX < 1000)
+                    {
+                        e.Complete();
+                        local = local + 1;
+                        marcador1.Text = local.ToString();
+                    }
+
+                    // Para detectar el gol encajado en la portería del equipo local
+                    if (ct.TranslateY >= -70 && ct.TranslateY < 50 && ct.TranslateX < -770 && ct.TranslateX >= -1000)
+                    {
+                        e.Complete();
+                        visitante = visitante + 1;
+                        marcador2.Text = visitante.ToString();
+                    }
                 }
-                */
             }
 
             image.RenderTransform = ct;
         }
 
-        private void grid_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        private void grid_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
         {
             CompositeTransform ct1 = new CompositeTransform();
+            
+            // Dimensión de la pantalla
             double width = grid.ActualWidth;
             double height = grid.ActualHeight;
+            
+            // Posición donde se hace el click derecho
             double positionX = e.GetPosition(grid).X;
             double positionY = e.GetPosition(grid).Y;
-            ct1.TranslateX = positionX - width / 2 + 115;
+            ct1.TranslateX = positionX - width / 2;
             ct1.TranslateY = positionY - height / 2;
-            
-            
+
+            // Creamos la nueva pelota
             Image img = new Image();
             img.Source = new BitmapImage(new Uri("ms-appx:///Assets/ball.png"));
             img.Width = 200;
             img.Height = 200;
             img.ManipulationMode = ManipulationModes.All;
             img.RenderTransform = ct1;
-
             img.ManipulationDelta += image_ManipulationDelta;
             img.ImageOpened += Image_ImageOpened;
 
-            listCTransform.Add(ct1);
+            //Añadimos la imagen y su CompositeTransform
             listImages.Add(img);
+            listCTransform.Add(ct1);
+            isBall.Add(true);
 
             grid.Children.Add(img);
-            
-            
+        }
+
+        private void grid_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            CompositeTransform ct1 = new CompositeTransform();
+
+            // Dimensión de la pantalla
+            double width = grid.ActualWidth;
+            double height = grid.ActualHeight;
+
+            // Posición donde se hace el click derecho
+            double positionX = e.GetPosition(grid).X;
+            double positionY = e.GetPosition(grid).Y;
+            ct1.TranslateX = positionX - width / 2;
+            ct1.TranslateY = positionY - height / 2;
+
+            // Creamos la nueva pelota
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri("ms-appx:///Assets/player.png"));
+            img.Width = 200;
+            img.Height = 200;
+            img.ManipulationMode = ManipulationModes.All;
+            img.RenderTransform = ct1;
+            img.ManipulationDelta += image_ManipulationDelta;
+            img.ImageOpened += Image_ImageOpened;
+
+            //Añadimos la imagen, su CompositeTransform y la 
+            listImages.Add(img);
+            listCTransform.Add(ct1);
+            isBall.Add(false);
+
+            grid.Children.Add(img);
         }
     }
 }
